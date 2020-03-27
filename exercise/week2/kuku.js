9f1147316e8f86a140d4949eb7ffe1eecb977e44
ayubwisesa <ayubluhungwisesa@gmail.com>
2020-03-28 03:46:22 +0700

    Create kuku.js

---
+++ exercise/week2/kuku.js
@@ -0,0 +1,12 @@
+var kuku = {
+    1: 'panjang',
+    2: 'PENDEK',
+    3: 'panjang'
+};
+for (kuku = 1; kuku <= 3; kuku++)
+    if (kuku = 'panjang') {
+        console.log(kuku);
+    }
+else {
+    console.log('puji');
+}
