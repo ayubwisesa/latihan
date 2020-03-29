7cd6acf3f2b063ac710a61d6bf1df70cdb52fbaa
ayubwisesa <ayubluhungwisesa@gmail.com>
2020-03-29 08:37:15 +0700

    Create exercise7.js

---
+++ exercise/week2/exercise7.js
@@ -0,0 +1,26 @@
+//soal no 1
+
+var row1 = 5;
+var x = 1;
+while (x <= row1) {
+    console.log("*")
+    x++
+}
+
+//soal no 2
+
+var row1 = 5;
+var x = 1;
+while (x <= row1) {
+    console.log("*".repeat(row1))
+    x++
+}
+
+//soal no 3
+
+var row1 = 5;
+var tempt = '';
+for (x = 0; x < row1; x++) {
+    tempt = x + 1;
+    console.log("*".repeat(tempt))
+}
