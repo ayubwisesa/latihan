709cd33e6ba10013291eadc894585cb7b0522c85
ayubwisesa <ayubluhungwisesa@gmail.com>
2020-04-05 20:52:28 +0700

    Create exercise6.js

---
+++ exercise/week3/exercise6.js
@@ -0,0 +1,19 @@
+function angkaPalindrome(num) {
+
+
+    let temp = ''
+    for (let i = num.toString().length - 1; i >= 0; i--) {
+        temp += num.toString()[i]
+    }
+
+    return num.toString() === temp ?
+        num :
+        angkaPalindrome(num)
+}
+
+// TEST CASES
+console.log(angkaPalindrome(8)); // 9
+console.log(angkaPalindrome(10)); // 11
+console.log(angkaPalindrome(117)); // 121
+console.log(angkaPalindrome(175)); // 181
+console.log(angkaPalindrome(1000)); // 1001
