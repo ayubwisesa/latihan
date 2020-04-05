33d8dbe13728ae13c6f0413e4523e7d6353f416d
ayubwisesa <ayubluhungwisesa@gmail.com>
2020-04-05 20:51:55 +0700

    Create exercise2.js

---
+++ exercise/week3/exercise2.js
@@ -0,0 +1,10 @@
+function balikString(kata) {
+
+    var temp = ''
+    for (var i = kata.length - 1; i >= 0; i--) {
+        temp += kata[i]
+    }
+    return temp
+}
+
+console.log(balikString('hello world!'))
