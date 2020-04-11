85599db6ec47ae147d599018d06f2e0123c4a263
ayubwisesa <ayubluhungwisesa@gmail.com>
2020-04-11 18:15:10 +0700

    Update exercise10.js

--- exercise/week4/exercise10.js
+++ exercise/week4/exercise10.js
@@ -1,19 +1,21 @@
 function changeMe(arr) {
     // you can only write your code here!
-    var date = new Date()
-    for (var i = 0; i < arr.length; i++) {
-        var temp = {
-            firstName: arr[i][0],
-            lastName: arr[i][1],
-            gender: arr[i][2],
-            age: arr[i][3] ? 2020 - arr[i][3] : 'Invalid Birth Year'
-        }
-        console.log((i + 1) + '.' + (temp.firstName) + ' ' + (temp.lastName) + ':')
+    if (!arr.length) {
+        console.log('""')
+    } else {
+        for (var i = 0; i < arr.length; i++) {
+            var temp = {
+                firstName: arr[i][0],
+                lastName: arr[i][1],
+                gender: arr[i][2],
+                age: arr[i][3] ? 2020 - arr[i][3] : 'Invalid Birth Year'
+            }
+            console.log((i + 1) + '.' + (temp.firstName) + ' ' + (temp.lastName) + ':')
 
-        console.log(temp)
+            console.log(temp)
+        }
     }
 }
-
 // TEST CASES
 changeMe([
     ['Christ', 'Evans', 'Male', 1982],
