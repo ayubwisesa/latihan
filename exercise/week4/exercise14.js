4c17c515d84e2b3867447a296a3d77f53f6825ac
ayubwisesa <ayubluhungwisesa@gmail.com>
2020-04-11 23:40:16 +0700

    Create exercise14.js

---
+++ exercise/week4/exercise14.js
@@ -0,0 +1,25 @@
+function naikAngkot(arrPenumpang) {
+    rute = ['A', 'B', 'C', 'D', 'E', 'F'];
+    //your code here
+    var arrObj = []
+    for (var penumpang of arrPenumpang) {
+        bayar = rute.indexOf(penumpang[2]) - rute.indexOf(penumpang[1])
+        var obj = {}
+        obj.penumpang = penumpang[0]
+        obj.naikDari = penumpang[1]
+        obj.tujuan = penumpang[2]
+        obj.bayar = bayar * 2000
+        arrObj.push(obj)
+    }
+    return arrObj;
+}
+
+//TEST CASE
+console.log(naikAngkot([
+    ['Dimitri', 'B', 'F'],
+    ['Icha', 'A', 'B']
+]));
+// [ { penumpang: 'Dimitri', naikDari: 'B', tujuan: 'F', bayar: 8000 },
+//   { penumpang: 'Icha', naikDari: 'A', tujuan: 'B', bayar: 2000 } ]
+
+console.log(naikAngkot([])); //[]
